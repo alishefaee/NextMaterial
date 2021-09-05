eba879679baf57d108c1774662a22577c950cb62
Brad <shafaid1@yahoo.com>
2021-09-05 10:03:28 +0430

    first commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+
+
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      Hello
+    </Container>
+  );
+}
